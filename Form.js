ee0d2d7e20b413e8d1b6e580e743da03a6ed4f59
Somya346 <74398874+Somya346@users.noreply.github.com>
2021-02-08 22:20:32 +0530

    Add files via upload

--- Form.js
+++ Form.js
@@ -1,34 +1,29 @@
 class Form{
     constructor(){
-        this.input =createInput("Name");
-        this.button = createButton('play');
-        this.greeting = createElement('h2');
-        this.title = createElement('h2');
-        this.reset = createButton('Reset')
+       this.input = createInput("Name");
+       this.button = createButton('Play');
+       this.greeting = createElement('h2');
+       this.title = createElement('h2');
     }
-    hide(){
+    hide() {
         this.greeting.hide();
         this.button.hide();
         this.input.hide();
         this.title.hide();
     }
-    display(){
+    display() {
         this.title.html("FRUIT CATCHER");
-        this.title.position(350 , 50);
-        this.title.style('font-size','70px');
+        this.title.position(350, 50);
+        this.title.style('font-size', '70px');
         this.title.style('color', 'skyblue');
-        this.input.position(550 , 400);
+        this.input.position(550,400);
         this.input.style('width', '200px');
         this.input.style('height', '20px');
-        this.input.style('background','lavender');
-        this.button.position(560 , 500);
-        this.button.style('width','200px');
-        this.button.style('height','40px');
+        this.input.style('background', 'lavender');
+        this.button.position(560,500);
+        this.button.style('width', '200px');
+        this.button.style('height', '40px');
         this.button.style('background', 'lightpink');
-        this.reset.position(50 , 560);
-        this.reset.style('width','100px');
-        this.reset.style('height','30px');
-        this.reset.style('background','lightpink');
 
         this.button.mousePressed(() => {
             this.input.hide();
@@ -38,19 +33,11 @@ class Form{
             player.index = playerCount;
             player.update();
             player.updateCount(playerCount);
-            this.greeting.html("Hello" + player.name)     
-            this.greeting.position(400 , 250);
-            this.greeting.style('color' , 'white');
-            this.greeting.style('font - size', '100px');
-        });
-        
-        this.reset.mousePressed(() =>{
-            player.updateCount(0);
-            game.update(0);
-
+            this.greeting.html("Hello " + player.name)
+            this.greeting.position(400,250);
+            this.greeting.style('color', 'white');
+            this.greeting.style('font-size', '100px');
         });
 
-        
-            
-        }
     }
+}
